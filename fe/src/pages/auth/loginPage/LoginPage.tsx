f4b86f1562bd5f2c52356bffeca05a8e7c7633f3
Kreativepardha <saradhipardha12@gmail.com>
2025-01-29 20:37:47 +0530

    .

---
+++ fe/src/pages/auth/loginPage/LoginPage.tsx
@@ -0,0 +1,14 @@
+import { LoginForm } from "../../../components/auth/login-components/loginform"
+
+function LoginPage() {
+
+  return (
+    <>
+    <div className="">
+        <LoginForm />
+    </div>
+    </>
+  )
+}
+
+export default LoginPage
